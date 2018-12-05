767ef7d0c3103cfb8f0ea8be95189bbbf1a8cc03
arpit-chandra <arpitchandra@gmail.com>
2018-12-05 00:26:59 -0500

    Test

---
+++ HelloOkta/Program.cs
@@ -0,0 +1,41 @@
+﻿using Okta.Core;
+using Okta.Core.Clients;
+using Okta.Core.Models;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HelloOkta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oktaClient = new OktaClient("00ty358R_ItCrIsZ7BxEA7dkPOJWtlc8Rt40A2JBBK", new Uri("https://piedmont.oktapreview.com"));
+            var usersClient = oktaClient.GetUsersClient();
+
+            try
+            {
+                int i = 1;
+                foreach (User user in usersClient)
+                {                    
+                    Console.WriteLine("User found - " + i++);
+                }
+            }
+            catch (OktaException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.Write("Please enter to End");
+                Console.Read();
+            }
+        }
+    }
+}
